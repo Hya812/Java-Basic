2c4fae77d9beb58b8adc6302a771c6a3806ecf09
Cẩm Tú <camtu120806@gmail.com>
2025-03-08 22:22:37 +0700

    EvenOrOddNumber.java

--- EvenOrOddNumber.java
+++ EvenOrOddNumber.java
@@ -1,4 +1,7 @@
-public class JavaBasic_1 {
+//Viết CT nhận 1 số nguyên bất kỳ và cho biết số đó là chẵn hay lẻ
+
+import java.util.Iterator;
+public class CheckEvenNumber{
 	public static String checkChanLe(int number) {
 		/*
 		 * Lap dieu kieu Neu chia het cho la so chan, nguoc lai la so le
