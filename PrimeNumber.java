6e70eafdebd9b714bca879d0fa26c754426f08e8
Cẩm Tú <camtu120806@gmail.com>
2025-03-08 22:21:25 +0700

    PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -5,7 +5,6 @@ public class PrimeNumber {
 	public static boolean checkSoNguyenTo(int number) {
 		// Khai bao bien count dung de dem cac uoc so cua nuber
 		int count = 0;
-		// numbe chi co 2 uoc so
 		// Vong lap dem cac uoc so
 		for (int i = 1; i <= number; i++) {
 			// Kiem tra uoc so
