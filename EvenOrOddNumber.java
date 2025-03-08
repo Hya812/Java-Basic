6b5c3d7d3857e52405f0ceef0d714614588661b2
Cẩm Tú <camtu120806@gmail.com>
2025-03-08 22:12:51 +0700

    EvenOrOddNumber.java

---
+++ EvenOrOddNumber.java
@@ -0,0 +1,10 @@
+public class JavaBasic_1 {
+	public static String checkChanLe(int number) {
+		/*
+		 * Lap dieu kieu Neu chia het cho la so chan, nguoc lai la so le
+		 */
+		if (number % 2 == 0) {
+			return "So chan";
+		}
+		return "So le";
+	}
