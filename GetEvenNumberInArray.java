4b2505d1d5afecbdc8a6c9b9fb8a36742cfaac77
Cẩm Tú <camtu120806@gmail.com>
2025-03-10 21:36:41 +0700

    GetEvenNumberInArray.java

---
+++ GetEvenNumberInArray.java
@@ -0,0 +1,11 @@
+public class GetEvenNumber {
+	public static void getEvenNumber(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] % 2 == 0) {
+				System.out.println(array[i]);
+			}
+		}
+
+	}
+
+}
